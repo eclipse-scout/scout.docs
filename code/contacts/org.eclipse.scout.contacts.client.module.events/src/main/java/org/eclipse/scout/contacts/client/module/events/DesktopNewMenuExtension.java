1f8fc192170dd1d13b71dcb9e378ad23e735b0b8
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2015-10-21 18:37:00 +0200

    Rename 'Events modules' of Contacts Application
    org.eclipse.scout.contacts.event.client > org.eclipse.scout.contacts.client.module.events
    org.eclipse.scout.contacts.event.shared > org.eclipse.scout.contacts.shared.module.events
    org.eclipse.scout.contacts.event.server > org.eclipse.scout.contacts.server.module.events

---
+++ code/contacts/org.eclipse.scout.contacts.client.module.events/src/main/java/org/eclipse/scout/contacts/client/module/events/DesktopNewMenuExtension.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.contacts.client.module.events;
+
+import java.util.Set;
+
+import org.eclipse.scout.commons.CollectionUtility;
+import org.eclipse.scout.commons.annotations.Order;
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.contacts.client.Desktop;
+import org.eclipse.scout.contacts.client.Desktop.FileMenu.NewMenu;
+import org.eclipse.scout.contacts.client.module.events.event.EventForm;
+import org.eclipse.scout.rt.client.extension.ui.action.menu.AbstractMenuExtension;
+import org.eclipse.scout.rt.client.ui.action.menu.AbstractMenu;
+import org.eclipse.scout.rt.client.ui.action.menu.IMenuType;
+import org.eclipse.scout.rt.shared.TEXTS;
+
+public class DesktopNewMenuExtension extends AbstractMenuExtension<Desktop.FileMenu.NewMenu> {
+
+  public DesktopNewMenuExtension(NewMenu owner) {
+    super(owner);
+  }
+
+  @Order(3000.0)
+  public class EventMenu extends AbstractMenu {
+
+    @Override
+    protected Set<? extends IMenuType> getConfiguredMenuTypes() {
+      return CollectionUtility.<IMenuType> hashSet();
+    }
+
+    @Override
+    protected String getConfiguredText() {
+      return TEXTS.get("Event");
+    }
+
+    @Override
+    protected void execAction() throws ProcessingException {
+      new EventForm().startNew();
+    }
+  }
+}
