32ad521474bd9e1f4d22883d9c1d3d300f33fcb2
Beat Schwarzentrub <bsh@bsiag.com>
2015-10-29 16:35:36 +0100

    Add demo for menus in tab boxes

--- code/widgets/org.eclipsescout.demo.widgets.shared/src/main/java/org/eclipsescout/demo/widgets/shared/Icons.java
+++ code/widgets/org.eclipsescout.demo.widgets.shared/src/main/java/org/eclipsescout/demo/widgets/shared/Icons.java
@@ -10,7 +10,9 @@
  ******************************************************************************/
 package org.eclipsescout.demo.widgets.shared;
 
-public class Icons {
+import org.eclipse.scout.rt.shared.AbstractIcons;
+
+public class Icons extends AbstractIcons {
   private static final long serialVersionUID = 1L;
 
   public static final String Alarmclock = "alarmclock";
