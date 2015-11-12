784883e78c2999bc4682b84d2475cc8be2d074c4
Daniel Wiehl <daniel.wiehl@bsi-software.com>
2015-11-12 16:20:34 +0100

    Change FormBasedAccessController to only be installed on UI server
    Also, changed name of Authenticators to AccessControllers. That is because an authenticator is not allowed to continue chain nor to change state of HTTP session. If only reading and verifying credentials, such an artifact is called Authenticator instead.

---
+++ code/contacts/org.eclipse.scout.contacts.server/src/main/java/org/eclipse/scout/contacts/server/ContactsServerServletFilter.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.contacts.server;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.server.commons.authentication.DevelopmentAccessController;
+import org.eclipse.scout.rt.server.commons.authentication.ServiceTunnelAccessTokenAccessController;
+import org.eclipse.scout.rt.server.commons.authentication.TrivialAccessController;
+import org.eclipse.scout.rt.server.commons.authentication.TrivialAccessController.TrivialAuthConfig;
+
+/**
+ * Backend servlet filter to protected resources from unauthenticated access.
+ *
+ * @since 5.1
+ */
+public class ContactsServerServletFilter implements Filter {
+
+  private TrivialAccessController m_trivialAccessController;
+  private ServiceTunnelAccessTokenAccessController m_tunnelAccessController;
+  private DevelopmentAccessController m_developmentAccessController;
+
+  @Override
+  public void init(final FilterConfig filterConfig) throws ServletException {
+    m_trivialAccessController = BEANS.get(TrivialAccessController.class).init(new TrivialAuthConfig().withExclusionFilter(filterConfig.getInitParameter("filter-exclude")));
+    m_tunnelAccessController = BEANS.get(ServiceTunnelAccessTokenAccessController.class).init();
+    m_developmentAccessController = BEANS.get(DevelopmentAccessController.class).init();
+  }
+
+  @Override
+  public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+    final HttpServletRequest req = (HttpServletRequest) request;
+    final HttpServletResponse resp = (HttpServletResponse) response;
+
+    if (m_trivialAccessController.handle(req, resp, chain)) {
+      return;
+    }
+
+    if (m_tunnelAccessController.handle(req, resp, chain)) {
+      return;
+    }
+
+    if (m_developmentAccessController.handle(req, resp, chain)) {
+      return;
+    }
+
+    resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+  }
+
+  @Override
+  public void destroy() {
+    m_developmentAccessController.destroy();
+    m_tunnelAccessController.destroy();
+    m_trivialAccessController.destroy();
+  }
+}
