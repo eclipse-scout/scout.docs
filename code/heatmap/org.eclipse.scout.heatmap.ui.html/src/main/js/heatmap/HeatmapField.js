fff4fb37bec042827f31c898a92d881febff5a8e
Beat Schwarzentrub <bsh@bsiag.com>
2015-12-28 16:01:59 +0100

    Improved HeatmapField.js
    Add status to fix right border.
    
    Fix layout error when canvas size gets too small. HeatmapFieldLayout.js
    now calls the public invalidateSize() method instead of the private
    _onResize().
    
    Add _remove() handling (cleanup listeners etc.).
    
    Cleanup code (remove unused variables, added missing semicolons at end
    of statements, use common method order, apply formatter).

--- code/heatmap/org.eclipse.scout.heatmap.ui.html/src/main/js/heatmap/HeatmapField.js
+++ code/heatmap/org.eclipse.scout.heatmap.ui.html/src/main/js/heatmap/HeatmapField.js
@@ -3,17 +3,11 @@ scout.HeatmapField = function() {
 };
 scout.inherits(scout.HeatmapField, scout.FormField);
 
-scout.HeatmapField.prototype._renderProperties = function() {
-  scout.HeatmapField.parent.prototype._renderProperties.call(this);
-  this._renderViewParameter();
-  this._renderHeatPointList();
-};
-
 scout.HeatmapField.prototype._render = function($parent) {
   this.addContainer($parent, 'heatmap-field');
   this.addLabel();
   this.addMandatoryIndicator();
-
+  this.addStatus();
 
   var heatmapId = scout.objectFactory.createUniqueId();
   var $field = this.$container
@@ -21,12 +15,17 @@ scout.HeatmapField.prototype._render = function($parent) {
     .attr('id', heatmapId);
   this.addField($field);
 
+  // Before (!) installing the layout, set the initial size to 1x1. The size of $field must not
+  // get smaller than that, because Leaflet.js throws an error when the drawing canvas has size 0.
+  // After the initial rendering, this condition is ensured by HeapmapFieldLayout.js.
+  scout.graphics.setSize($field, new scout.Dimension(1, 1));
   var fieldHtmlComp = new scout.HtmlComponent($field, this.session);
-  fieldHtmlComp.setSize(new scout.Dimension(1,1));
   fieldHtmlComp.setLayout(new scout.HeatmapFieldLayout(this));
 
-  this.heatmap = L.map(heatmapId, {trackResize: false});
-  var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  this.heatmap = L.map(heatmapId, {
+    trackResize: false
+  });
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(this.heatmap);
 
@@ -34,7 +33,20 @@ scout.HeatmapField.prototype._render = function($parent) {
   this.heatmap.on('moveend', this._handleViewParameterChanged.bind(this));
   this.heatmap.on('click', this._handleClicked.bind(this));
   this.heatmap.on('contextmenu', this._handleClicked.bind(this));
+};
 
+scout.HeatmapField.prototype._renderProperties = function() {
+  scout.HeatmapField.parent.prototype._renderProperties.call(this);
+  this._renderViewParameter();
+  this._renderHeatPointList();
+};
+
+scout.HeatmapField.prototype._remove = function() {
+  scout.HeatmapField.parent.prototype._remove.call(this);
+
+  this.heatmap.remove();
+  this.heatmap = null;
+  this._heatLayer = null;
 };
 
 scout.HeatmapField.prototype._handleViewParameterChanged = function() {
@@ -57,19 +69,21 @@ scout.HeatmapField.prototype._handleClicked = function(event) {
 };
 
 scout.HeatmapField.prototype._renderViewParameter = function() {
-    this.heatmap.setView([this.viewParameter.center.x, this.viewParameter.center.y],this.viewParameter.zoomFactor);
+  this.heatmap.setView([this.viewParameter.center.x, this.viewParameter.center.y], this.viewParameter.zoomFactor);
 };
 
 scout.HeatmapField.prototype._renderHeatPointList = function() {
-  if(this._heatLayer) {
+  if (this._heatLayer) {
     this.heatmap.removeLayer(this._heatLayer);
   }
-  this._heatLayer = L.heatLayer(this.heatPointList, {radius: 20})
+  this._heatLayer = L.heatLayer(this.heatPointList, {
+    radius: 20
+  });
   this._heatLayer.addTo(this.heatmap);
 };
 
 scout.HeatmapField.prototype._onHeatPointsAdded = function(points) {
-  if(this._heatLayer) {
+  if (this._heatLayer) {
     for (i = 0; i < points.length; i++) {
       this._heatLayer.addLatLng(points[i]);
     }
