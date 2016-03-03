bd1a5b5a5a0f1f7b75cb21ec3b60cd98a767d187
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2016-03-03 15:38:20 +0100

    Add license header to modules

--- code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/js/heatmap-module.js
+++ code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/js/heatmap-module.js
@@ -1,3 +1,11 @@
+/*!
+* Eclipse Scout Heatmap
+* https://eclipse.org/scout/
+*
+* Copyright (c) BSI Business Systems Integration AG. All rights reserved.
+* Released under the Eclipse Public License v1.0
+* http://www.eclipse.org/legal/epl-v10.html
+*/
 (function(scout, $, undefined) {
   __include("heatmap/HeatmapField.js");
   __include("heatmap/HeatmapFieldLayout.js");
