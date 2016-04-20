f32878dec245ac0c5d130b172a01e1347b89942b
Jeremie Bresson <jeremie.bresson@bsi-software.com>
2016-04-20 08:38:08 +0200

    Improve Platform section

--- code/org.eclipse.scout.docs.snippets/src/test/java/org/eclipse/scout/docs/snippets/platform/config/ApplicationNameConstant.java
+++ code/org.eclipse.scout.docs.snippets/src/test/java/org/eclipse/scout/docs/snippets/platform/config/ApplicationNameConstant.java
@@ -1,10 +1,13 @@
 package org.eclipse.scout.docs.snippets.platform.config;
 
+//tag::code[]
 import org.eclipse.scout.rt.platform.IgnoreBean;
 import org.eclipse.scout.rt.platform.Replace;
 import org.eclipse.scout.rt.platform.config.PlatformConfigProperties.ApplicationNameProperty;
 
+//end::code[]
 @IgnoreBean
+//tag::code[]
 @Replace
 public class ApplicationNameConstant extends ApplicationNameProperty {
   @Override
@@ -12,3 +15,4 @@ public class ApplicationNameConstant extends ApplicationNameProperty {
     return "Contacts Application";
   }
 }
+//end::code[]
--- code/org.eclipse.scout.docs.snippets/src/test/java/org/eclipse/scout/docs/snippets/platform/config/ApplicationNamePropertyRedirection.java
+++ code/org.eclipse.scout.docs.snippets/src/test/java/org/eclipse/scout/docs/snippets/platform/config/ApplicationNamePropertyRedirection.java
@@ -1,10 +1,13 @@
 package org.eclipse.scout.docs.snippets.platform.config;
 
+//tag::code[]
 import org.eclipse.scout.rt.platform.IgnoreBean;
 import org.eclipse.scout.rt.platform.Replace;
 import org.eclipse.scout.rt.platform.config.PlatformConfigProperties.ApplicationNameProperty;
 
+//end::code[]
 @IgnoreBean
+//tag::code[]
 @Replace
 public class ApplicationNamePropertyRedirection extends ApplicationNameProperty {
 
@@ -13,3 +16,4 @@ public class ApplicationNamePropertyRedirection extends ApplicationNameProperty 
     return "myproject.application.name";
   }
 }
+//end::code[]
