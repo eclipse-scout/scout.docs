3a215cbd7b702177628776e989fd5dc7624b830f
Ivan Motsch <ivan.motsch@bsiag.com>
2016-06-29 15:04:49 +0200

    Strong Content Security Policy
    inline-javascript was only used in html files. These scripts are now
    externalized in a file with the same name as the html file in the
    corresponding res folder.
    The html file references this script file in the head section.

---
+++ code/widgets/org.eclipse.scout.widgets.ui.html.app/src/main/resources/WebContent/res/login.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  scout.login.init({texts: <scout:message style="javascript" key="ui.Login" key="ui.LoginFailed" key="ui.User" key="ui.Password" /> });
+});
---
+++ code/widgets/org.eclipse.scout.widgets.ui.html.app/src/main/resources/WebContent/res/logout.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  scout.logout.init({texts: <scout:message style="javascript" key="ui.LogoutSuccessful" key="ui.LoginAgain" /> });
+});
