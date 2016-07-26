7975ea767998db598c8fa888edb9751a05038f5f
Matthias Villiger <mvi@bsi-software.com>
2016-07-26 16:40:28 +0200

    Remove duplicate ClassId

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TreeFieldForm.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TreeFieldForm.java
@@ -469,7 +469,7 @@ public class TreeFieldForm extends AbstractForm implements IAdvancedExampleForm 
           }
 
           @Order(15)
-          @ClassId("b43eaa82-5241-4688-b8f9-4a32ddd44f83")
+          @ClassId("378e162c-e2f4-4688-89ff-b433e75cf8d7")
           public class DeleteNodeMenu extends AbstractMenu {
             @Override
             protected String getConfiguredText() {
