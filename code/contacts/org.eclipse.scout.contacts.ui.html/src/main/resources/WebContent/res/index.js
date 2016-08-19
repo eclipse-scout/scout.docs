902f13e511ea7a2d9610da1aa36b14569a2f14b6
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2016-08-19 18:53:44 +0200

    Updated textproviderservices and index.js

--- code/contacts/org.eclipse.scout.contacts.ui.html/src/main/resources/WebContent/res/index.js
+++ code/contacts/org.eclipse.scout.contacts.ui.html/src/main/resources/WebContent/res/index.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+<<<<<<< Upstream, based on branch 'releases/6.1.x' of https://github.com/BSI-Business-Systems-Integration-AG/org.eclipse.scout.docs
     var app = new scout.RemoteApp();
     app.init({
+=======
+  var app = new scout.RemoteApp();
+  app.init({
+>>>>>>> 799951d Updated textproviderservices and index.js
     bootstrap: {
       fonts: ['scoutIcons']
     }
