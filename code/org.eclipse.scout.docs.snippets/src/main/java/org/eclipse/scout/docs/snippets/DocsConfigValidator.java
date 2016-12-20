45f54c6a83b4f8049e150829bda2814fd8d61b88
Matthias Villiger <mvi@bsi-software.com>
2016-12-20 16:27:27 +0100

    Fix test case

---
+++ code/org.eclipse.scout.docs.snippets/src/main/java/org/eclipse/scout/docs/snippets/DocsConfigValidator.java
@@ -0,0 +1,19 @@
+package org.eclipse.scout.docs.snippets;
+
+import org.eclipse.scout.rt.platform.config.IConfigurationValidator;
+
+/**
+ * <h3>{@link DocsConfigValidator}</h3>
+ */
+public class DocsConfigValidator implements IConfigurationValidator {
+
+  @Override
+  public boolean isValid(String key, String value) {
+    // the config.properties contains an example entry with this key. It should be ignored in validation.
+    return "myproject.application.name".equals(key);
+  }
+
+  @Override
+  public void dispose() {
+  }
+}
