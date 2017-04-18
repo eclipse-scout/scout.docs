a8ede928ce113a52cca6fcabf6b548ffc9d73f7c
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2017-04-18 15:43:00 +0200

    Bench layout documentation
    Change-Id: Id68cb2fde1bb86a107c2c57325c9ac4fabebeeed

---
+++ code/org.eclipse.scout.docs.snippets/src/main/java/org/eclipse/scout/docs/snippets/BenchLayoutSnippet.java
@@ -0,0 +1,29 @@
+package org.eclipse.scout.docs.snippets;
+
+import org.eclipse.scout.rt.client.ui.desktop.IDesktop;
+import org.eclipse.scout.rt.client.ui.desktop.bench.layout.BenchColumnData;
+import org.eclipse.scout.rt.client.ui.desktop.bench.layout.BenchLayoutData;
+import org.eclipse.scout.rt.client.ui.desktop.bench.layout.FlexboxLayoutData;
+
+/**
+ * <h3>{@link BenchLayoutSnippet}</h3>
+ *
+ * @author aho
+ */
+public class BenchLayoutSnippet {
+
+  @SuppressWarnings("null")
+  void example() {
+    IDesktop desktop = null;
+    // tag::BenchLayoutSample[]
+    desktop.setBenchLayoutData( // <1>
+        new BenchLayoutData()
+            .withCacheKey("a-cache-key") // <2>
+            .withCenter( // <3>
+                new BenchColumnData()
+                    .withNorth(new FlexboxLayoutData().withGrow(0).withShrink(0).withInitial(280).withRelative(false)) // <4>
+                    .withCenter(new FlexboxLayoutData()) // <5>
+                    .withSouth(new FlexboxLayoutData().withShrink(0).withInitial(-1)))); // <6>
+    // tag::BenchLayoutSample[]
+  }
+}
