e545c71d936adf790c94cbcc2ddadca101b2faab
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-04-28 11:01:57 +0200

    CarouselForm.js: use images and add status enabled checkbox

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/CarouselForm.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/CarouselForm.js
@@ -7,16 +7,32 @@ jswidgets.CarouselForm.prototype._init = function(model) {
   jswidgets.CarouselForm.parent.prototype._init.call(this, model);
 
   var bodyGrid = new scout.HorizontalGroupBoxBodyGrid();
+  bodyGrid.validate(this.rootGroupBox);
+  bodyGrid = new scout.HorizontalGroupBoxBodyGrid();
   bodyGrid.validate(this.widget('DetailBox'));
+  bodyGrid = new scout.HorizontalGroupBoxBodyGrid();
+  bodyGrid.validate(this.widget('PropertiesBox'));
 
-  scout.arrays.ensure(this.widget('Carousel').widgets).forEach(function(widget) {
+  var carousel = this.widget('Carousel');
+  scout.arrays.ensure(carousel.widgets).forEach(function(widget) {
     if (widget.rootGroupBox) {
       var grid = new scout.HorizontalGroupBoxBodyGrid();
       grid.validate(widget.rootGroupBox);
     }
   });
+
+  var statusEnabledField = this.widget('StatusEnabledField');
+  statusEnabledField.setValue(carousel.statusEnabled);
+  statusEnabledField.on('propertyChange', this._onStatusEnabledPropertyChange.bind(this));
 };
 
 jswidgets.CarouselForm.prototype._jsonModel = function() {
   return scout.models.getModel('jswidgets.CarouselForm');
+};
+
+jswidgets.CarouselForm.prototype._onStatusEnabledPropertyChange = function(event) {
+  if (scout.arrays.containsAny(event.changedProperties, ['value'])) {
+    var enabled = event.source.value;
+    this.widget('Carousel').setStatusEnabled(enabled);
+  }
 };
