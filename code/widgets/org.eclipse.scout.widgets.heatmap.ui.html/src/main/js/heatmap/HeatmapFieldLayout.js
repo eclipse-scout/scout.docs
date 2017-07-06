4672c7187147cdd7861f6814be665b4440908f65
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2017-07-06 14:56:22 +0200

    Graphics: use consistent method naming
    Rename getBounds to cssBounds.
    Rename getMargins to margins.
    Rename getInsets to insets.
    Rename getSize to size.
    
    Also done for htmlComponent:
    Rename getBounds to cssBounds.
    Rename getMargins to margins.
    Rename getInsets to insets.
    Rename getSize to size.
    Rename getPreferredSize to prefSize.
    Rename getAvailableSize to availableSize.

--- code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/js/heatmap/HeatmapFieldLayout.js
+++ code/widgets/org.eclipse.scout.widgets.heatmap.ui.html/src/main/js/heatmap/HeatmapFieldLayout.js
@@ -13,7 +13,7 @@ scout.HeatmapFieldLayout.prototype.layout = function($container) {
   // Because of a bug (?) in Leaflet.js, the canvas size must not get smaller than
   // 1x1 pixels, otherwise an exception is thrown: "Failed to execute 'getImageData'
   // on 'CanvasRenderingContext2D': The source width is 0."
-  var size = htmlContainer.getSize();
+  var size = htmlContainer.size();
   if (size.width === 0 || size.height === 0) {
     size.width = Math.max(size.width, 1);
     size.height = Math.max(size.height, 1);
