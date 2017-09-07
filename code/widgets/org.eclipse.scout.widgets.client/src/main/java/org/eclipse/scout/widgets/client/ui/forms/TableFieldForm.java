842b7571c616cb11aff4cbcfebe6907e979e2b7c
Rudolf Schreier <Rudolf.Schreier@bsi-software.com>
2017-09-07 14:43:33 +0200

    Fix Copy&Paste error

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TableFieldForm.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TableFieldForm.java
@@ -1702,7 +1702,6 @@ public class TableFieldForm extends AbstractForm implements IAdvancedExampleForm
           @Override
           protected void execInitField() {
             setValue(getTableField().getTable().isMultiSelect());
-            setValue(getTableField().getTable().isHeaderVisible());
           }
         }
 
