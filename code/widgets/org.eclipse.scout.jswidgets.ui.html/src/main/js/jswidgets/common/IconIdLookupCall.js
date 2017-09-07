63d45404da42eae74c301dc14878caad2a490277
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2017-09-07 15:44:39 +0200

    Fix for IE.
    Change-Id: I60253d601986f66fbec38bac8c632509095cabad

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/common/IconIdLookupCall.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/common/IconIdLookupCall.js
@@ -15,11 +15,11 @@ scout.inherits(jswidgets.IconIdLookupCall, scout.StaticLookupCall);
 
 jswidgets.IconIdLookupCall.prototype._data = function() {
   return Object.keys(scout.icons)
-  .filter(function(name) {
-    var value = scout.icons[name];
-    return typeof value === 'string' && value.startsWith('font:');
-  }).map(function(name, i) {
-    var iconId = scout.icons[name];
-    return [iconId, name];
-  });
+    .filter(function(name) {
+      var value = scout.icons[name];
+      return typeof value === 'string' && scout.strings.startsWith(value, 'font:');
+    }).map(function(name, i) {
+      var iconId = scout.icons[name];
+      return [iconId, name];
+    });
 };
