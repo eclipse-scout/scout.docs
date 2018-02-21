f4b6c1d27199405a79becbb27a678832dcf770c5
Beat Schwarzentrub <bsh@bsiag.com>
2018-02-21 10:43:28 +0100

    Automatic Preloading of Web Fonts
    Describe mechanism in releases notes and migration guide.
    
    Widgets app:
    Remove static delaration of web fonts to preload from App.init()
    bootstrap argument. The fonts to preload are now detected automatically.

--- code/widgets/org.eclipse.scout.jswidgets.ui.html.app/src/main/resources/WebContent/res/index.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html.app/src/main/resources/WebContent/res/index.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   var app = new jswidgets.App();
   app.init({
     bootstrap: {
-      fonts: ['scoutIcons'],
       modelsUrl: 'res/jswidgets-all.json',
       textsUrl: 'res/texts.json'
     }
