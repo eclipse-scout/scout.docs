4c539653a19b2867f3e5f9c9bb24e2a1a86ae436
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2018-03-02 16:53:46 +0100

    Widgets: use form field properties box

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TagFieldForm.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/TagFieldForm.java
@@ -14,7 +14,6 @@ import java.util.Arrays;
 
 import org.eclipse.scout.rt.client.ui.form.AbstractForm;
 import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
-import org.eclipse.scout.rt.client.ui.form.fields.booleanfield.AbstractBooleanField;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractButton;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractCloseButton;
 import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
@@ -24,12 +23,10 @@ import org.eclipse.scout.rt.platform.classid.ClassId;
 import org.eclipse.scout.rt.shared.TEXTS;
 import org.eclipse.scout.rt.shared.services.lookup.ILookupCall;
 import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.CloseButton;
-import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ConfigurationBox;
-import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ConfigurationBox.EnabledField;
 import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ExamplesBox;
 import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ExamplesBox.DefaultField;
 import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ExamplesBox.DisabledField;
-import org.eclipse.scout.widgets.client.ui.forms.TagFieldForm.MainBox.ExamplesBox.MandatoryField;
+import org.eclipse.scout.widgets.client.ui.template.formfield.AbstractFormFieldPropertiesBox;
 
 public class TagFieldForm extends AbstractForm implements IPageForm {
 
@@ -60,20 +57,8 @@ public class TagFieldForm extends AbstractForm implements IPageForm {
     return getFieldByClass(DisabledField.class);
   }
 
-  public ConfigurationBox getConfigurationBox() {
-    return getFieldByClass(ConfigurationBox.class);
-  }
-
   public MainBox getMainBox() {
     return getFieldByClass(MainBox.class);
-  }
-
-  public EnabledField getEnabledField() {
-    return getFieldByClass(EnabledField.class);
-  }
-
-  public MandatoryField getMandatoryField() {
-    return getFieldByClass(MandatoryField.class);
   }
 
   @Order(10)
@@ -99,28 +84,14 @@ public class TagFieldForm extends AbstractForm implements IPageForm {
         protected Class<? extends ILookupCall<String>> getConfiguredLookupCall() {
           return TagsLookupCall.class;
         }
-      }
-
-      @Order(20)
-      public class MandatoryField extends AbstractTagField {
 
         @Override
-        protected String getConfiguredLabel() {
-          return TEXTS.get("Mandatory");
-        }
-
-        @Override
-        protected boolean getConfiguredMandatory() {
-          return true;
-        }
-
-        @Override
-        protected int getConfiguredGridH() {
+        protected int getConfiguredGridW() {
           return 2;
         }
       }
 
-      @Order(30)
+      @Order(20)
       public class DisabledField extends AbstractTagField {
 
         @Override
@@ -134,37 +105,33 @@ public class TagFieldForm extends AbstractForm implements IPageForm {
         }
 
         @Override
+        protected int getConfiguredGridW() {
+          return 2;
+        }
+
+        @Override
         protected void execInitField() {
           setTags(Arrays.asList("frühling", "gemeinsam bsi 2017", "prinzessin"));
         }
       }
     }
 
-    @Order(20)
-    public class ConfigurationBox extends AbstractGroupBox {
-
+    @Order(300)
+    @ClassId("eb1abced-79fd-4c31-b795-9074fab96134")
+    public class FormFieldPropertiesBox extends AbstractFormFieldPropertiesBox {
       @Override
       protected String getConfiguredLabel() {
-        return TEXTS.get("Configure");
+        return "Form Field Properties";
       }
 
-      @Order(1000)
-      @ClassId("e2b37a46-6e43-4f5e-9bfe-f0eddf31f123")
-      public class EnabledField extends AbstractBooleanField {
-        @Override
-        protected String getConfiguredLabel() {
-          return "Enabled";
-        }
+      @Override
+      protected void execInitField() {
+        setFormField(getDefaultField());
+      }
 
-        @Override
-        protected void execChangedValue() {
-          getDisabledField().setEnabled(getValue());
-        }
-
-        @Override
-        protected void execInitField() {
-          setValue(false);
-        }
+      @Override
+      protected boolean getConfiguredExpanded() {
+        return true;
       }
     }
 
