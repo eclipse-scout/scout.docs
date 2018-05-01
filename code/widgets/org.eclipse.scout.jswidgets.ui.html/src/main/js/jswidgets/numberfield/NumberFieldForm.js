db389e64c5827faa1880dcdf1ac81897b03a4b37
Matthias Villiger <mvi@bsi-software.com>
2018-05-01 13:09:09 +0200

    Support minimum & maximum value for NumberFields in Scout JS
    The NumberField in ScoutJS now supports defining minimum & maximum
    values. The same feature already exists in Scout Classic and is now
    available in ScoutJS as well.

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/numberfield/NumberFieldForm.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/numberfield/NumberFieldForm.js
@@ -24,6 +24,15 @@ jswidgets.NumberFieldForm.prototype._init = function(model) {
   var formatField = this.widget('FormatField');
   formatField.setValue(numberField.decimalFormat.pattern);
   formatField.on('propertyChange', this._onFormatPropertyChange.bind(this));
+
+  var minValueField = this.widget('MinValueField');
+  minValueField.on('propertyChange', this._onMinValueFieldPropertyChange.bind(this));
+  minValueField.setValue(numberField.minValue);
+
+  var maxValueField = this.widget('MaxValueField');
+  maxValueField.on('propertyChange', this._onMaxValueFieldPropertyChange.bind(this));
+  maxValueField.setValue(numberField.maxValue);
+
   this.widget('ValueField').setEnabled(true);
   this.widget('ValueFieldPropertiesBox').setField(numberField);
   this.widget('FormFieldPropertiesBox').setField(numberField);
@@ -33,5 +42,17 @@ jswidgets.NumberFieldForm.prototype._init = function(model) {
 jswidgets.NumberFieldForm.prototype._onFormatPropertyChange = function(event) {
   if (event.propertyName === 'value') {
     this.widget('NumberField').setDecimalFormat(event.newValue);
+  }
+};
+
+jswidgets.NumberFieldForm.prototype._onMinValueFieldPropertyChange = function(event) {
+  if (event.propertyName === 'value') {
+    this.widget('NumberField').setMinValue(event.newValue);
+  }
+};
+
+jswidgets.NumberFieldForm.prototype._onMaxValueFieldPropertyChange = function(event) {
+  if (event.propertyName === 'value') {
+    this.widget('NumberField').setMaxValue(event.newValue);
   }
 };
