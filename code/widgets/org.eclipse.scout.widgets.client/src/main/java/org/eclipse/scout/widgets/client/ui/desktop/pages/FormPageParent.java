f2a8f2c202a19f92aec3ed05198ffba5a4a1431f
Beat Schwarzentrub <bsh@bsiag.com>
2018-05-10 19:08:41 +0200

    Fix JavaDoc

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/desktop/pages/FormPageParent.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/desktop/pages/FormPageParent.java
@@ -6,11 +6,11 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import org.eclipse.scout.widgets.client.deeplink.FormDeepLinkHandler;
+import org.eclipse.scout.widgets.client.deeplink.FormPageDeepLinkHandler;
 
 /**
  * Marker for pages that may contain {@link IFormPage}s as children. Useful when resolving deep links, see
- * {@link FormDeepLinkHandler}.
+ * {@link FormPageDeepLinkHandler}.
  */
 @Retention(RUNTIME)
 @Target(TYPE)
