schifferle/org.eclipse.scout.docs

Widgets: add tile type to TileGridForm to choose between new tiles
Also renamed SimpleTile to CustomTile to make it more clear.

code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/tilegrid/CustomTile.js
@@ -1,0 +1,24 @@
+jswidgets.CustomTile = function() {
+  jswidgets.CustomTile.parent.call(this);
+  this.label = null;
+  this.displayStyle = scout.Tile.DisplayStyle.PLAIN;
+};
+scout.inherits(jswidgets.CustomTile, scout.Tile);
+
+jswidgets.CustomTile.prototype._render = function() {
+  this.$container = this.$parent.appendDiv('simple-tile');
+  this.htmlComp = scout.HtmlComponent.install(this.$container, this.session);
+};
+
+jswidgets.CustomTile.prototype._renderProperties = function() {
+  jswidgets.CustomTile.parent.prototype._renderProperties.call(this);
+  this._renderLabel();
+};
+
+jswidgets.CustomTile.prototype.setLabel = function(label) {
+  this.setProperty('label', label);
+};
+
+jswidgets.CustomTile.prototype._renderLabel = function() {
+  this.$container.text(this.label);
+};

code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/widgets/tile/CustomTile.js
@@ -1,0 +1,19 @@
+widgets.CustomTile = function() {
+  widgets.CustomTile.parent.call(this);
+  this.label = null;
+};
+scout.inherits(widgets.CustomTile, scout.Tile);
+
+widgets.CustomTile.prototype._render = function() {
+  this.$container = this.$parent.appendDiv('simple-tile');
+  this.htmlComp = scout.HtmlComponent.install(this.$container, this.session);
+};
+
+widgets.CustomTile.prototype._renderProperties = function() {
+  widgets.CustomTile.parent.prototype._renderProperties.call(this);
+  this._renderLabel();
+};
+
+widgets.CustomTile.prototype._renderLabel = function() {
+  this.$container.text(this.label);
+};

eclipse-scout/scout.docs

code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/tilegrid/CustomTile.js
@@ -1,0 +1,24 @@
+jswidgets.CustomTile = function() {
+  jswidgets.CustomTile.parent.call(this);
+  this.label = null;
+  this.displayStyle = scout.Tile.DisplayStyle.PLAIN;
+};
+scout.inherits(jswidgets.CustomTile, scout.Tile);
+
+jswidgets.CustomTile.prototype._render = function() {
+  this.$container = this.$parent.appendDiv('custom-tile');
+  this.htmlComp = scout.HtmlComponent.install(this.$container, this.session);
+};
+
+jswidgets.CustomTile.prototype._renderProperties = function() {
+  jswidgets.CustomTile.parent.prototype._renderProperties.call(this);
+  this._renderLabel();
+};
+
+jswidgets.CustomTile.prototype.setLabel = function(label) {
+  this.setProperty('label', label);
+};
+
+jswidgets.CustomTile.prototype._renderLabel = function() {
+  this.$container.text(this.label);
+};

code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/widgets/tile/CustomTile.js
@@ -1,0 +1,19 @@
+widgets.CustomTile = function() {
+  widgets.CustomTile.parent.call(this);
+  this.label = null;
+};
+scout.inherits(widgets.CustomTile, scout.Tile);
+
+widgets.CustomTile.prototype._render = function() {
+  this.$container = this.$parent.appendDiv('custom-tile');
+  this.htmlComp = scout.HtmlComponent.install(this.$container, this.session);
+};
+
+widgets.CustomTile.prototype._renderProperties = function() {
+  widgets.CustomTile.parent.prototype._renderProperties.call(this);
+  this._renderLabel();
+};
+
+widgets.CustomTile.prototype._renderLabel = function() {
+  this.$container.text(this.label);
+};