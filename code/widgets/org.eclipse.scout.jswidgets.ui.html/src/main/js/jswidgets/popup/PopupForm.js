36e860a52f2215803c81492c2797aaedda7138cb
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2018-10-29 19:01:34 +0100

    PopupForm: add some more properties

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/popup/PopupForm.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/popup/PopupForm.js
@@ -11,6 +11,7 @@
 jswidgets.PopupForm = function() {
   jswidgets.PopupForm.parent.call(this);
   this.$popupAnchor = null;
+  this.popup = null;
 };
 scout.inherits(jswidgets.PopupForm, scout.Form);
 
@@ -29,23 +30,42 @@ jswidgets.PopupForm.prototype._init = function(model) {
   var anchorBoundsField = this.widget('AnchorBoundsField');
   anchorBoundsField.on('propertyChange', this._onAnchorBoundsChange.bind(this));
 
+  var closeOnAnchorMouseDownField = this.widget('CloseOnAnchorMouseDownField');
+  closeOnAnchorMouseDownField.setValue(dummyPopup.closeOnAnchorMouseDown);
+
+  var closeOnMouseDownOutsideField = this.widget('CloseOnMouseDownOutsideField');
+  closeOnMouseDownOutsideField.setValue(dummyPopup.closeOnMouseDownOutside);
+
+  var closeOnOtherPopupOpenField = this.widget('CloseOnOtherPopupOpenField');
+  closeOnOtherPopupOpenField.setValue(dummyPopup.closeOnOtherPopupOpen);
+
   var horizontalAlignmentField = this.widget('HorizontalAlignmentField');
   horizontalAlignmentField.setValue(dummyPopup.horizontalAlignment);
+  horizontalAlignmentField.on('propertyChange', this._onHorizontalAlignmentPropertyChange.bind(this));
 
   var verticalAlignmentField = this.widget('VerticalAlignmentField');
   verticalAlignmentField.setValue(dummyPopup.verticalAlignment);
-
-  var trimWidthField = this.widget('TrimWidthField');
-  trimWidthField.setValue(dummyPopup.trimWidth);
-
-  var trimHeightField = this.widget('TrimHeightField');
-  trimHeightField.setValue(dummyPopup.trimHeight);
+  verticalAlignmentField.on('propertyChange', this._onVerticalAlignmentPropertyChange.bind(this));
 
   var horizontalSwitchField = this.widget('HorizontalSwitchField');
   horizontalSwitchField.setValue(dummyPopup.horizontalSwitch);
+  horizontalSwitchField.on('propertyChange', this._onHorizontalSwitchPropertyChange.bind(this));
 
   var verticalSwitchField = this.widget('VerticalSwitchField');
   verticalSwitchField.setValue(dummyPopup.verticalSwitch);
+  verticalSwitchField.on('propertyChange', this._onVerticalSwitchPropertyChange.bind(this));
+
+  var trimWidthField = this.widget('TrimWidthField');
+  trimWidthField.setValue(dummyPopup.trimWidth);
+  trimWidthField.on('propertyChange', this._onTrimWidthPropertyChange.bind(this));
+
+  var trimHeightField = this.widget('TrimHeightField');
+  trimHeightField.setValue(dummyPopup.trimHeight);
+  trimHeightField.on('propertyChange', this._onTrimHeightPropertyChange.bind(this));
+
+  var withArrowField = this.widget('WithArrowField');
+  withArrowField.setValue(dummyPopup.withArrow);
+  withArrowField.on('propertyChange', this._onWithArrowPropertyChange.bind(this));
 
   dummyPopup.close();
 };
@@ -69,16 +89,20 @@ jswidgets.PopupForm.prototype._onOpenPopupButtonClick = function(model) {
     $anchor = this.widget('OpenPopupButton').$field;
   }
   var anchorBounds = this._getAnchorBounds();
-  var popup = scout.create('WidgetPopup', {
+  this.popup = scout.create('WidgetPopup', {
     parent: this,
     $anchor: $anchor,
     anchorBounds: anchorBounds,
+    closeOnAnchorMouseDown: this.widget('CloseOnAnchorMouseDownField').value,
+    closeOnMouseDownOutside: this.widget('CloseOnMouseDownOutsideField').value,
+    closeOnOtherPopupOpen: this.widget('CloseOnOtherPopupOpenField').value,
     horizontalAlignment: this.widget('HorizontalAlignmentField').value,
     verticalAlignment: this.widget('VerticalAlignmentField').value,
     trimWidth: this.widget('TrimWidthField').value,
     trimHeight: this.widget('TrimHeightField').value,
     horizontalSwitch: this.widget('HorizontalSwitchField').value,
     verticalSwitch: this.widget('VerticalSwitchField').value,
+    withArrow: this.widget('WithArrowField').value,
     cssClass: 'popup-form-popup',
     widget: {
       objectType: "jswidgets.Text",
@@ -89,9 +113,9 @@ jswidgets.PopupForm.prototype._onOpenPopupButtonClick = function(model) {
           '<p>The popup will be as big as its content, so you probably need to define its size, e.g. using the min- and max-width CSS properties.</p>'
     }
   });
-  this.widget('EventsTab').setField(popup);
-  this.widget('WidgetActionsBox').setField(popup);
-  popup.open();
+  this.widget('EventsTab').setField(this.popup);
+  this.widget('WidgetActionsBox').setField(this.popup);
+  this.popup.open();
 };
 
 jswidgets.PopupForm.prototype._onAnchorBoundsChange = function(event) {
@@ -118,5 +142,47 @@ jswidgets.PopupForm.prototype._updateAnchor = function() {
       this.$popupAnchor = this.session.$entryPoint.appendDiv('popup-anchor');
     }
     scout.graphics.setBounds(this.$popupAnchor, anchorBounds);
+  }
+};
+
+jswidgets.PopupForm.prototype._onHorizontalAlignmentPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setHorizontalAlignment(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onVerticalAlignmentPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setVerticalAlignment(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onHorizontalSwitchPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setHorizontalSwitch(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onTrimWidthPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setTrimWidth(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onTrimHeightPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setTrimHeight(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onVerticalSwitchPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setVerticalSwitch(event.newValue);
+  }
+};
+
+jswidgets.PopupForm.prototype._onWithArrowPropertyChange = function(event) {
+  if (event.propertyName === 'value' && this.popup) {
+    this.popup.setWithArrow(event.newValue);
   }
 };
