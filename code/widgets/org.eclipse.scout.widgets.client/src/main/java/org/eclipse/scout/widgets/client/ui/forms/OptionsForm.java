cbd9156be4a227e379edfdc34a7a4cfa523e6b20
Ralf Muri <ralf.muri@bsi-software.com>
2019-01-22 15:24:05 +0100

    add dense mode to OptionsForm

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/OptionsForm.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/OptionsForm.java
@@ -10,16 +10,20 @@
  ******************************************************************************/
 package org.eclipse.scout.widgets.client.ui.forms;
 
+import org.eclipse.scout.rt.client.ui.desktop.IDesktop;
 import org.eclipse.scout.rt.client.ui.form.AbstractForm;
 import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractCancelButton;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractOkButton;
+import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractRadioButton;
 import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
+import org.eclipse.scout.rt.client.ui.form.fields.radiobuttongroup.AbstractRadioButtonGroup;
 import org.eclipse.scout.rt.client.ui.form.fields.smartfield.AbstractSmartField;
 import org.eclipse.scout.rt.platform.Order;
 import org.eclipse.scout.rt.platform.text.TEXTS;
 import org.eclipse.scout.rt.platform.util.ObjectUtility;
 import org.eclipse.scout.rt.shared.services.common.code.ICodeType;
+import org.eclipse.scout.widgets.client.ui.forms.OptionsForm.MainBox.GroupBox.DisplayStyleRadioButtonGroup;
 import org.eclipse.scout.widgets.client.ui.forms.OptionsForm.MainBox.GroupBox.UiThemeField;
 import org.eclipse.scout.widgets.shared.services.code.UiThemeCodeType;
 import org.eclipse.scout.widgets.shared.services.code.UiThemeCodeType.DefaultCode;
@@ -35,6 +39,8 @@ public class OptionsForm extends AbstractForm {
   protected void execInitForm() {
     String theme = ObjectUtility.nvl(getDesktop().getTheme(), DefaultCode.ID);
     getUiThemeField().setValue(theme);
+    String displayStyle = ObjectUtility.nvl(getDesktop().getDisplayStyle(), IDesktop.DISPLAY_STYLE_DEFAULT);
+    getDisplayStyleRadioButtonGroup().setValue(displayStyle);
   }
 
   public void startNew() {
@@ -43,6 +49,10 @@ public class OptionsForm extends AbstractForm {
 
   public MainBox getMainBox() {
     return getFieldByClass(MainBox.class);
+  }
+
+  public DisplayStyleRadioButtonGroup getDisplayStyleRadioButtonGroup() {
+    return getFieldByClass(DisplayStyleRadioButtonGroup.class);
   }
 
   public UiThemeField getUiThemeField() {
@@ -78,6 +88,44 @@ public class OptionsForm extends AbstractForm {
           return true;
         }
       }
+
+      @Order(20)
+      public class DisplayStyleRadioButtonGroup extends AbstractRadioButtonGroup<String> {
+
+        @Override
+        protected String getConfiguredLabel() {
+          return TEXTS.get("Layout");
+        }
+
+        @Order(10)
+        public class DefaultButton extends AbstractRadioButton<String> {
+
+          @Override
+          protected String getConfiguredLabel() {
+            return TEXTS.get("Default");
+          }
+
+          @Override
+          protected String getConfiguredRadioValue() {
+            return IDesktop.DISPLAY_STYLE_DEFAULT;
+          }
+        }
+
+        @Order(20)
+        public class DenseButton extends AbstractRadioButton<String> {
+
+          @Override
+          protected String getConfiguredLabel() {
+            return TEXTS.get("Dense");
+          }
+
+          @Override
+          protected String getConfiguredRadioValue() {
+            return IDesktop.DISPLAY_STYLE_DENSE;
+          }
+        }
+
+      }
     }
 
     @Order(10)
@@ -93,7 +141,12 @@ public class OptionsForm extends AbstractForm {
 
     @Override
     protected void execStore() {
-      getDesktop().setTheme(getUiThemeField().getValue());
+      if (getUiThemeField().isSaveNeeded()) {
+        getDesktop().setTheme(getUiThemeField().getValue());
+      }
+      if (getDisplayStyleRadioButtonGroup().isSaveNeeded()) {
+        getDesktop().setDisplayStyle(getDisplayStyleRadioButtonGroup().getValue());
+      }
     }
   }
 }
