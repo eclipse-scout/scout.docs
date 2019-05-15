a34825150de088c8182e4cf009bbe7a23f64ed93
Ralf Muri <ralf.muri@bsi-software.com>
2019-05-15 16:44:05 +0200

    add simple menus to SamplePageWithTable

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/page/SamplePageWithTableDetailTable.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/page/SamplePageWithTableDetailTable.js
@@ -3,6 +3,13 @@ jswidgets.SamplePageWithTableDetailTable = function() {
 };
 scout.inherits(jswidgets.SamplePageWithTableDetailTable, scout.Table);
 
+jswidgets.SamplePageWithTableDetailTable.prototype._init = function(model) {
+  jswidgets.SamplePageWithTableDetailTable.parent.prototype._init.call(this, model);
+
+  this.widget('AddRowMenu').on('action', this._onAddRowMenuAction.bind(this));
+  this.widget('DeleteRowMenu').on('action', this._onDeleteRowMenuAction.bind(this));
+};
+
 jswidgets.SamplePageWithTableDetailTable.prototype.createTileForRow = function(row) {
   var model = {
     parent: this,
@@ -13,4 +20,30 @@ jswidgets.SamplePageWithTableDetailTable.prototype.createTileForRow = function(r
       row.data.bool
   };
   return new scout.create('HtmlTile', model);
+};
+
+jswidgets.SamplePageWithTableDetailTable.prototype._onAddRowMenuAction = function() {
+  this.insertRow(this._createRow());
+};
+
+jswidgets.SamplePageWithTableDetailTable.prototype._onDeleteRowMenuAction = function() {
+  this.deleteRows(this.selectedRows);
+};
+
+jswidgets.SamplePageWithTableDetailTable.prototype._createRow = function() {
+  var row = {
+    id: this.rows.length + 1,
+    string: 'string ' + (this.rows.length + 1),
+    number: Math.floor(Math.random() * Math.floor(999999)),
+    bool: Math.random() >= 0.5
+  };
+  return {
+    data: row,
+    cells: [
+      row.id,
+      row.string,
+      row.number,
+      row.bool
+    ]
+  };
 };
