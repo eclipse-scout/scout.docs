33ba497877ab07413a0721d4847f967864b79c33
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2019-12-03 09:54:58 +0100

    Import @eclipse-scout/svg
    Otherwise SvgField and Adapter would not be included in the bundle.

--- code/widgets/org.eclipse.scout.widgets.ui.html.app/src/main/js/index.js
+++ code/widgets/org.eclipse.scout.widgets.ui.html.app/src/main/js/index.js
@@ -9,10 +9,11 @@
  *     BSI Business Systems Integration AG - initial API and implementation
  */
 import {RemoteApp} from '@eclipse-scout/core';
+import * as ref0 from '@eclipse-scout/svg';
 import * as ref1 from '@eclipse-scout/demo-widgets';
 import * as ref2 from '@eclipse-scout/demo-widgets-heatmap';
 
-Object.assign({}, ref1, ref2); // workaround so that the imports are not unused
+Object.assign({}, ref0, ref1, ref2); // workaround so that the imports are not unused
 
 new RemoteApp().init({
   bootstrap: {
