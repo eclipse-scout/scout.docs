d5180f807f744ee841a68ed3612eb8046e9f7a06
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2019-12-18 08:13:41 +0100

    Added missing ClassId annotations

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/ImageFieldForm.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/forms/ImageFieldForm.java
@@ -15,6 +15,7 @@ import java.net.URL;
 import java.util.List;
 
 import org.eclipse.scout.rt.client.dto.FormData;
+import org.eclipse.scout.rt.client.dto.FormData.SdkCommand;
 import org.eclipse.scout.rt.client.ui.dnd.ResourceListTransferObject;
 import org.eclipse.scout.rt.client.ui.dnd.TransferObject;
 import org.eclipse.scout.rt.client.ui.form.AbstractForm;
@@ -52,6 +53,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Order(7000.0)
+@ClassId("0fde5f3a-513a-45ce-9957-cb0c2cdb21fd")
 public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm {
 
   private static final Logger LOG = LoggerFactory.getLogger(ImageFieldForm.class);
@@ -117,6 +119,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
   }
 
   @Order(10)
+  @ClassId("f6ee8c17-1166-470c-8f92-4252ddbc4dc5")
   public class MainBox extends AbstractGroupBox {
 
     public static final String SCOUT_LOGO = "images/eclipse_scout_logo.png";
@@ -130,6 +133,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
     }
 
     @Order(10)
+    @ClassId("0b717ac1-4d0b-43e9-8b8f-f8ba9e3eef96")
     public class ExamplesBox extends AbstractGroupBox {
 
       @Override
@@ -143,6 +147,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(10)
+      @ClassId("40c78cb8-034c-46b5-a6fc-a60f973e0033")
       public class DefaultField extends AbstractImageField {
 
         @Override
@@ -205,7 +210,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
 
       @Order(20)
       @ClassId("7e8b10d2-799c-488c-ae63-d352eec5d62f")
-      @FormData(sdkCommand = FormData.SdkCommand.IGNORE)
+      @FormData(sdkCommand = SdkCommand.IGNORE)
       public class AlignmentTitleField extends AbstractLabelField {
 
         @Override
@@ -230,6 +235,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(100)
+      @ClassId("20919b94-de17-4e07-b843-798590c23e2c")
       public class IconContentFieldTopLeft extends AbstractAlignedImageField {
         public IconContentFieldTopLeft() {
           super(-1, -1);
@@ -237,6 +243,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(110)
+      @ClassId("e2eeca37-9bb6-49d6-82fd-787e0a5eb9ed")
       public class IconContentFieldMiddleLeft extends AbstractAlignedImageField {
         public IconContentFieldMiddleLeft() {
           super(0, -1);
@@ -244,6 +251,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(120)
+      @ClassId("c0074d4f-a849-4cdf-9306-0a3d414057fb")
       public class IconContentFieldBottomLeft extends AbstractAlignedImageField {
         public IconContentFieldBottomLeft() {
           super(1, -1);
@@ -251,6 +259,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(200)
+      @ClassId("aba02980-8e1c-46d6-8f91-f3c7bb07b445")
       public class IconContentFieldTopCenter extends AbstractAlignedImageField {
         public IconContentFieldTopCenter() {
           super(-1, 0);
@@ -258,6 +267,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(210)
+      @ClassId("66decb40-2edf-4d4a-a7bb-1fe01722a1dd")
       public class IconContentFieldMiddleCenter extends AbstractAlignedImageField {
         public IconContentFieldMiddleCenter() {
           super(0, 0);
@@ -265,6 +275,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(220)
+      @ClassId("42ec888a-1f99-4711-93f4-c01636eec591")
       public class IconContentFieldBottomCenter extends AbstractAlignedImageField {
         public IconContentFieldBottomCenter() {
           super(1, 0);
@@ -272,6 +283,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(300)
+      @ClassId("8ca4b731-015d-4db0-8ff8-6521f61081ab")
       public class IconContentFieldTopRight extends AbstractAlignedImageField {
         public IconContentFieldTopRight() {
           super(-1, 1);
@@ -279,6 +291,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(310)
+      @ClassId("cac64b35-d0a7-4699-b343-a28e672f05fb")
       public class IconContentFieldMiddleRight extends AbstractAlignedImageField {
         public IconContentFieldMiddleRight() {
           super(0, 1);
@@ -286,6 +299,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(320)
+      @ClassId("c21627f3-0f05-4ccd-8871-18eb2ec61ccf")
       public class IconContentFieldBottomRight extends AbstractAlignedImageField {
         public IconContentFieldBottomRight() {
           super(1, 1);
@@ -294,6 +308,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
     }
 
     @Order(20)
+    @ClassId("81229c47-583b-4bda-ab5f-d35fb3f2aa1a")
     public class ConfigurationBox extends AbstractGroupBox {
 
       @Override
@@ -307,6 +322,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(10)
+      @ClassId("b052fcb0-0d55-4154-af0f-8c1afdf0bcd2")
       public class Image1Field extends AbstractImageField {
 
         @Override
@@ -331,6 +347,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(15)
+      @ClassId("39b4788f-817b-4e03-a211-91f201b14c3e")
       public class Image1ScrollBarEnabledCheckbox extends AbstractBooleanField {
 
         @Override
@@ -356,6 +373,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(16)
+      @ClassId("867ccc24-8a25-476a-b640-21ed740316bb")
       public class Image1AutoFitCheckbox extends AbstractBooleanField {
 
         @Override
@@ -381,6 +399,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(20)
+      @ClassId("07d4987d-9830-4a44-bceb-db877d67084a")
       public class Image2Field extends AbstractImageField {
 
         @Override
@@ -415,6 +434,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(25)
+      @ClassId("faeca908-4a51-434f-8863-e875c17a0b80")
       public class Image2ScrollbarEnabledFieldCheckbox extends AbstractBooleanField {
 
         @Override
@@ -440,6 +460,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(26)
+      @ClassId("db830613-5367-49c6-a16e-64c08b21e583")
       public class Image2AutoFitCheckbox extends AbstractBooleanField {
 
         @Override
@@ -465,6 +486,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(30)
+      @ClassId("9f2052e0-2ea3-40c4-b508-65ef881c0323")
       public class ImageURLField extends AbstractStringField {
 
         @Override
@@ -522,6 +544,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
       }
 
       @Order(40)
+      @ClassId("dc2b867e-fa27-4221-a8c1-a84de3dc6737")
       public class ImageIdField extends AbstractSmartField<String> {
 
         @Override
@@ -564,6 +587,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
     }
 
     @Order(30)
+    @ClassId("665ca770-0c69-48d6-97fc-fd442edbfda6")
     public class SampleContentButton extends AbstractButton {
 
       @Override
@@ -578,6 +602,7 @@ public class ImageFieldForm extends AbstractForm implements IAdvancedExampleForm
     }
 
     @Order(40)
+    @ClassId("56d326d5-a1b6-4b04-9367-4ad4e15c0e37")
     public class CloseButton extends AbstractCloseButton {
     }
   }
