54d39b4a717f891e31211c352a3b39badbf2fdc5
Cyrill Wyss <cyrill.wyss@bsi-software.com>
2020-09-14 14:47:25 +0200

    Stage 1 migration during merge remotes/origin/releases/9.0.x to releases/10.0

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/breadcrumbbar/BreadcrumbBarFieldForm.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets/breadcrumbbar/BreadcrumbBarFieldForm.js
@@ -1,3 +1,6 @@
+import {Form, MessageBoxes, Status, arrays, models} from '@eclipse-scout/core';
+import BreadcrumbBarFieldFormModel from './BreadcrumbBarFieldFormModel';
+
 /*******************************************************************************
  * Copyright (c) 2017 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
@@ -8,54 +11,56 @@
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
-jswidgets.BreadcrumbBarFieldForm = function() {
-  jswidgets.BreadcrumbBarFieldForm.parent.call(this);
+export default class BreadcrumbBarFieldForm extends Form {
 
-  this.breadcrumbBarField = null;
-  this._breacrumbActionListener = this._onBreadcrumbAction.bind(this);
-};
-scout.inherits(jswidgets.BreadcrumbBarFieldForm, scout.Form);
+  constructor() {
+    super();
 
-jswidgets.BreadcrumbBarFieldForm.prototype._jsonModel = function() {
-  return scout.models.getModel('jswidgets.BreadcrumbBarFieldForm');
-};
-
-jswidgets.BreadcrumbBarFieldForm.prototype._onBreadcrumbAction = function(event) {
-  scout.MessageBoxes.openOk(this, this.session.text('BreadcrumbClickedX', event.source.ref), scout.Status.Severity.INFO);
-};
-
-jswidgets.BreadcrumbBarFieldForm.prototype._init = function(model) {
-  jswidgets.BreadcrumbBarFieldForm.parent.prototype._init.call(this, model);
-
-  this.breadcrumbBarField = this.widget('BreadcrumbBarField');
-
-  this.widget('FormFieldPropertiesBox').setField(this.breadcrumbBarField);
-  this.widget('GridDataBox').setField(this.breadcrumbBarField);
-  this.widget('WidgetActionsBox').setField(this.breadcrumbBarField);
-  this.widget('EventsTab').setField(this.breadcrumbBarField);
-
-  this.widget('BreadcrumbItemsField').on('propertyChange', this._onBreadcrumbItemsPropertyChange.bind(this));
-  this.widget('BreadcrumbItemsField').setValue('Storyboard\nFolder\nChild Folder');
-};
-
-jswidgets.BreadcrumbBarFieldForm.prototype._onBreadcrumbItemsPropertyChange = function(event) {
-  if (event.propertyName !== 'value') {
-    return;
+    this.breadcrumbBarField = null;
+    this._breacrumbActionListener = this._onBreadcrumbAction.bind(this);
   }
 
-  this.breadcrumbBarField.breadcrumbBar.breadcrumbItems.forEach(function(item) {
-    item.off('action', this._breacrumbActionListener);
-  }.bind(this));
+  _jsonModel() {
+    return models.get(BreadcrumbBarFieldFormModel);
+  }
 
-  this.breadcrumbBarField.setBreadcrumbItems(scout.arrays.ensure(event.newValue.split("\n")).map(function(text) {
-    return {
-      objectType: 'BreadcrumbItem',
-      text: text,
-      ref: text
-    };
-  }.bind(this)));
+  _onBreadcrumbAction(event) {
+    MessageBoxes.openOk(this, this.session.text('BreadcrumbClickedX', event.source.ref), Status.Severity.INFO);
+  }
 
-  this.breadcrumbBarField.breadcrumbBar.breadcrumbItems.forEach(function(item) {
-    item.on('action', this._breacrumbActionListener);
-  }.bind(this));
-};
+  _init(model) {
+    super._init(model);
+
+    this.breadcrumbBarField = this.widget('BreadcrumbBarField');
+
+    this.widget('FormFieldPropertiesBox').setField(this.breadcrumbBarField);
+    this.widget('GridDataBox').setField(this.breadcrumbBarField);
+    this.widget('WidgetActionsBox').setField(this.breadcrumbBarField);
+    this.widget('EventsTab').setField(this.breadcrumbBarField);
+
+    this.widget('BreadcrumbItemsField').on('propertyChange', this._onBreadcrumbItemsPropertyChange.bind(this));
+    this.widget('BreadcrumbItemsField').setValue('Storyboard\nFolder\nChild Folder');
+  }
+
+  _onBreadcrumbItemsPropertyChange(event) {
+    if (event.propertyName !== 'value') {
+      return;
+    }
+
+    this.breadcrumbBarField.breadcrumbBar.breadcrumbItems.forEach(function(item) {
+      item.off('action', this._breacrumbActionListener);
+    }.bind(this));
+
+    this.breadcrumbBarField.setBreadcrumbItems(arrays.ensure(event.newValue.split("\n")).map(function(text) {
+      return {
+        objectType: 'BreadcrumbItem',
+        text: text,
+        ref: text
+      };
+    }.bind(this)));
+
+    this.breadcrumbBarField.breadcrumbBar.breadcrumbItems.forEach(function(item) {
+      item.on('action', this._breacrumbActionListener);
+    }.bind(this));
+  }
+}
