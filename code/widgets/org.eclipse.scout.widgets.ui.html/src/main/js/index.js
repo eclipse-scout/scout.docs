ca1e63fee779c0be6f18f326fbacfad3d77761bb
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2020-12-23 13:30:02 +0100

    Remove ChartField
    Will be replaced by scout chart field.

--- code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/index.js
+++ code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/index.js
@@ -5,8 +5,6 @@ export {default as WidgetsOutlineAdapter} from './WidgetsOutlineAdapter';
 export {default as WidgetsTileOutlineOverview} from './WidgetsTileOutlineOverview';
 export {default as CustomTile} from './tile/CustomTile';
 export {default as CustomTileAdapter} from './tile/CustomTileAdapter';
-export {default as ChartField} from './custom/chartfield/ChartField';
-export {default as ChartFieldAdapter} from './custom/chartfield/ChartFieldAdapter';
 
 import * as self from './index.js';
 
