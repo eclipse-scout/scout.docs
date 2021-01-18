f645ee71f04976d71cfd3e26f6b1aefcf2073441
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2021-01-18 16:57:07 +0100

    Add ChartFieldForm.js

---
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/chartfield/ChartTypeLookupCall.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
+import {StaticLookupCall} from '@eclipse-scout/core';
+import {Chart} from '@eclipse-scout/chart';
+
+export default class ChartTypeLookupCall extends StaticLookupCall {
+
+  constructor() {
+    super();
+  }
+
+  _data() {
+    return ChartTypeLookupCall.DATA;
+  }
+
+  static DATA = [
+    [Chart.Type.BAR, 'Bar'],
+    [Chart.Type.BAR_HORIZONTAL, 'Bar Horizontal'],
+    [Chart.Type.BUBBLE, 'Bubble'],
+    [Chart.Type.COMBO_BAR_LINE, 'Combo Bar Line'],
+    [Chart.Type.DOUGHNUT, 'Doughnut'],
+    [Chart.Type.FULFILLMENT, 'Fulfillment'],
+    [Chart.Type.LINE, 'Line'],
+    [Chart.Type.PIE, 'Pie'],
+    [Chart.Type.POLAR_AREA, 'Polar area'],
+    [Chart.Type.RADAR, 'Radar'],
+    [Chart.Type.SALESFUNNEL, 'Salesfunnel'],
+    [Chart.Type.SPEEDO, 'Speedo'],
+    [Chart.Type.VENN, 'Venn']
+  ];
+}
