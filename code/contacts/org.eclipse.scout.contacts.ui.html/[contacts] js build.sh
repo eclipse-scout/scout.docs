8f87c37fa94c2c13e6c7b81133998e34024de30f
Matthias Villiger <mvi@bsi-software.com>
2021-02-19 10:54:15 +0100

    Update Scout documentation URL

--- code/contacts/org.eclipse.scout.contacts.ui.html/[contacts] js build.sh
+++ code/contacts/org.eclipse.scout.contacts.ui.html/[contacts] js build.sh
@@ -5,13 +5,16 @@
 #
 # It has to be run once before the UI server is started.
 # You need to rerun it, if you update your JS dependencies (package.json).
-# Please see the Scout documentation for details about the available run scripts: https://eclipsescout.github.io/10.0/technical-guide-js.html#command-line-interface-cli
+# Please see the Scout documentation for details about the available run scripts: https://eclipsescout.github.io/22.0/technical-guide-js.html#command-line-interface-cli
 #
 # To make this script work you need a current version of Node.js (>=12.1.0) and npm (>=6.9.0).
 # Node.js (incl. npm) is available here: https://nodejs.org/.
 
 # Check if npm is available
-command -v npm >/dev/null 2>&1 || { echo >&2 "npm cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details. Aborting."; exit 1; }
+command -v npm >/dev/null 2>&1 || {
+  echo >&2 "npm cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details. Aborting."
+  exit 1
+}
 
 # Install pnpm
 echo Installing 'pnpm' into ../../node_modules
