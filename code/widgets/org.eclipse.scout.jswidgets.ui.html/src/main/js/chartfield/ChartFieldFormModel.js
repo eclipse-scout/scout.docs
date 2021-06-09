023f17c1c8a37892e5420603285360c53f86f7ce
Fritz Schinkel <Fritz.Schinkel@bsi-software.com>
2021-06-09 13:44:11 +0200

    BrushUp: update dashboard tile style
     - change tile style (border-radius, shadow, colors, gaps)

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/chartfield/ChartFieldFormModel.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/chartfield/ChartFieldFormModel.js
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
 import {FormField, icons} from '@eclipse-scout/core';
 
 export default () => ({
@@ -65,6 +75,7 @@ export default () => ({
                 tileGrid: {
                   id: 'TileGrid',
                   objectType: 'TileGrid',
+                  scrollable: false,
                   tiles: [
                     {
                       id: 'ChartTile',
--- code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/WidgetsTileOutlineOverview.js
+++ code/widgets/org.eclipse.scout.widgets.ui.html/src/main/js/WidgetsTileOutlineOverview.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2014-2019 BSI Business Systems Integration AG.
+ * Copyright (c) 2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/org/documents/edl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -54,5 +54,9 @@ export default class WidgetsTileOutlineOverview extends TileOutlineOverview {
         rowHeight: 90
       }
     });
+  }
+
+  _updateTitle() {
+    // NOP
   }
 }
