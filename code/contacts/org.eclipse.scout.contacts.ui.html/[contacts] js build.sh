9b59a8515483d613409acfb9016bdc72cd5ceaa2
Matthias Villiger <mvi@bsi-software.com>
2022-02-14 09:14:04 +0100

    Update Node & pnpm requirements

--- code/contacts/org.eclipse.scout.contacts.ui.html/[contacts] js build.sh
+++ code/contacts/org.eclipse.scout.contacts.ui.html/[contacts] js build.sh
@@ -1,25 +1,27 @@
-#! /bin/sh
+#!/bin/sh
 
 # This script installs all the JS dependencies and builds the JavaScript and CSS bundles.
 # It also starts a watcher which triggers a rebuild of these bundles whenever JS or CSS code changes.
 #
 # It has to be run once before the UI server is started.
-# You need to rerun it, if you update your JS dependencies (package.json).
+# You need to rerun it if you update your JS dependencies (package.json).
 # Please see the Scout documentation for details about the available run scripts: https://eclipsescout.github.io/22.0/technical-guide-js.html#command-line-interface-cli
 #
-# To make this script work you need a current version of Node.js (>=12.1.0) and npm (>=6.9.0).
+# To make this script work you need a current version of Node.js (>=16.13.0), npm (>=8.1.0) and pnpm (>=6.22.2).
 # Node.js (incl. npm) is available here: https://nodejs.org/.
+# pnpm is available here: https://pnpm.io/
 
-# Check if npm is available
-command -v npm >/dev/null 2>&1 || {
-  echo >&2 "npm cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details. Aborting."
-  exit 1
-}
+# Abort the script if any command fails
+set -e
 
-# Install pnpm
-echo Installing 'pnpm' into ../../node_modules
-npm install pnpm@">=5.0.0 <6.0.0" --prefix "../../"
-echo
+# Specify the path to the node and npm binaries
+PATH=$PATH:/usr/local/bin
+
+# Check if node is available
+command -v node >/dev/null 2>&1 || { echo >&2 "npm cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details."; exit 1; }
+
+# Check if pnpm is available
+command -v pnpm >/dev/null 2>&1 || { echo >&2 "pnpm cannot be found. Make sure pnpm is installed. See the content of this script for details."; exit 1; }
 
 # Install all JavaScript dependencies defined in the package.json => creates the node_modules folder
 cd ../..
--- code/widgets/org.eclipse.scout.widgets.ui.html.app/[widgets] js build.cmd
+++ code/widgets/org.eclipse.scout.widgets.ui.html.app/[widgets] js build.cmd
@@ -4,22 +4,26 @@
 :: It also starts a watcher which triggers a rebuild of these bundles whenever JS or CSS code changes.
 ::
 :: It has to be run once before the UI server is started.
-:: You need to rerun it, if you update your JS dependencies (package.json).
+:: You need to rerun it if you update your JS dependencies (package.json).
 :: Please see the Scout documentation for details about the available run scripts: https://eclipsescout.github.io/22.0/technical-guide-js.html#command-line-interface-cli
 ::
-::Â To make this script work you need a current version of Node.js (>=12.1.0) and npm (>=6.9.0).
-::Â Node.js (incl. npm) is available here: https://nodejs.org/.
+:: To make this script work you need a current version of Node.js (>=16.13.0), npm (>=8.1.0) and pnpm (>=6.22.2).
+:: Node.js (incl. npm) is available here: https://nodejs.org/.
+:: pnpm is available here: https://pnpm.io/
 
-::Â Check if npm is available
-where npm >nul 2>nul
+:: Check if node is available
+where node >nul 2>nul
 if %errorlevel% neq 0 (
-  echo npm cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details. 1>&2
+  echo node cannot be found. Make sure Node.js is installed and the PATH variable correctly set. See the content of this script for details. 1>&2
   exit /b 1
 )
 
-:: Install pnpm
-echo Installing 'pnpm' into ../../node_modules
-call npm install pnpm@">=5.0.0 <6.0.0" --prefix ../../node_modules
+:: Check if pnpm is available
+where pnpm >nul 2>nul
+if %errorlevel% neq 0 (
+  echo pnpm cannot be found. Make sure pnpm is installed. See the content of this script for details. 1>&2
+  exit /b 1
+)
 
 :: Install all JavaScript dependencies defined in the package.json => creates the node_modules folder
 call cd ..\..
