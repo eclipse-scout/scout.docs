b069309d8cdeda749c834ec69b876d24aed35bc5
Matthias Villiger <mvi@bsi-software.com>
2022-04-26 15:42:13 +0200

    Migration to Scout 22

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This repository contains the files needed to build and run a Scout JS Hello World application.
 
 It is intended to be used with the following tutorial:
-https://eclipsescout.github.io/11.0/helloscout-js.html
+https://eclipsescout.github.io/22.0/helloscout-js.html
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const baseConfig = require('@eclipse-scout/cli/scripts/webpack-defaults');
 module.exports = (env, args) => {
-  args.resDirArray = ['./res', './node_modules/@eclipse-scout/core/res'];
+  args.resDirArray = [
+    './res', 
+    './node_modules/@eclipse-scout/core/res',
+	'./node_modules/@eclipse-scout/core/dist/locales.json',
+	'./node_modules/@eclipse-scout/core/dist/texts.json'
+  ];
   const config = baseConfig(env, args);
   config.entry = {
     'helloworld': './src/helloworld.js',
