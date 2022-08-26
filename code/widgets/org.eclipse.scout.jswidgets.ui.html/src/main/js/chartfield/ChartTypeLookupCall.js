80cd68cf65e3b22733d238e1aa47891018b29a7a
Timon Gygax <timon.gygax@bsi-software.com>
2022-08-26 15:59:48 +0200

    Add scatter chart type to chart field

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/chartfield/ChartTypeLookupCall.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/chartfield/ChartTypeLookupCall.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2017 BSI Business Systems Integration AG.
+ * Copyright (c) 2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/org/documents/edl-v10.html
+ * https://www.eclipse.org/org/documents/edl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -33,6 +33,7 @@ export default class ChartTypeLookupCall extends StaticLookupCall {
     [Chart.Type.POLAR_AREA, 'Polar area'],
     [Chart.Type.RADAR, 'Radar'],
     [Chart.Type.SALESFUNNEL, 'Salesfunnel'],
+    [Chart.Type.SCATTER, 'Scatter'],
     [Chart.Type.SPEEDO, 'Speedo'],
     [Chart.Type.VENN, 'Venn']
   ];
