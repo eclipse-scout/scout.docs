ffcfad4a8172b34a9b23ff4603539be9983023db
Matthias Villiger <mvi@bsi-software.com>
2022-12-02 16:37:20 +0100

    Revert "Temporary workaround for https://github.com/smallrye/jandex/issues/289"
    This reverts commit ecf4d63848c8499dbcc09a2b3367ad4b3d3babae.
    
    Workaround no longer required because Jandex has been updated to 3.0.5

--- code/contacts/org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/Desktop.java
+++ code/contacts/org.eclipse.scout.contacts.client/src/main/java/org/eclipse/scout/contacts/client/Desktop.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2022 BSI Business Systems Integration AG.
+ * Copyright (c) 2015 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
- * https://www.eclipse.org/org/documents/edl-v10.html
+ * http://www.eclipse.org/org/documents/edl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -66,14 +66,14 @@ public class Desktop extends AbstractDesktop {
   // outline buttons of the application
   @Order(1)
   @ClassId("9405937b-66e8-491a-831d-69adca724b90")
-  public class ContactsOutlineViewButton extends AbstractOutlineViewButton {
+  public class ContactOutlineViewButton extends AbstractOutlineViewButton {
     //end::DesktopInit[]
 
-    public ContactsOutlineViewButton() {
+    public ContactOutlineViewButton() {
       this(ContactOutline.class);
     }
 
-    protected ContactsOutlineViewButton(Class<? extends ContactOutline> outlineClass) {
+    protected ContactOutlineViewButton(Class<? extends ContactOutline> outlineClass) {
       super(Desktop.this, outlineClass);
     }
 
