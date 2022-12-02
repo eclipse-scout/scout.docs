e22533a56ca220a6fe8bea0118664c2556ad4e4a
Matthias Villiger <mvi@bsi-software.com>
2022-12-02 15:41:18 +0100

    Cleanup package.json & documentation versions

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/desktop/DesktopModel.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/desktop/DesktopModel.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ */
 import {icons, WidgetsOutline} from '../index';
 import {Desktop, DesktopModel, icons as icons_1, Menu} from '@eclipse-scout/core';
 
