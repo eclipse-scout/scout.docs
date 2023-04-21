b42b00cc84c4747bfa78e639d5c46f04c22826d9
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2023-04-21 10:31:27 +0200

    JsWidgets: migrate LookupCallLookupCall

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/lookup/LookupCallLookupCall.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/lookup/LookupCallLookupCall.ts
@@ -7,47 +7,25 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {arrays, LookupCall, LookupResult, LookupRow, QueryBy, scout, StaticLookupCall} from '@eclipse-scout/core';
+import {arrays, LookupCall, LookupRow, StaticLookupCall} from '@eclipse-scout/core';
 import {LocaleLookupCall, RainbowLookupCall, SalutationLookupCall, WorldLookupCall} from '../index';
-import Deferred = JQuery.Deferred;
 
 export class LookupCallLookupCall extends StaticLookupCall<LookupCall<any>> {
 
-  constructor() {
-    super();
-  }
-
-  protected override _queryByKey(deferred: Deferred<LookupResult<LookupCall<any>>>, key: LookupCall<any>) {
-    if (key instanceof LookupCall) {
-      deferred.resolve({
-        queryBy: QueryBy.KEY,
-        lookupRows: [{
-          key: key,
-          text: key.objectType as string,
-          enabled: true
-        } as LookupRow<LookupCall<any>>]
-      });
-      return;
+  protected override _lookupRowByKey(key: LookupCall<any>): LookupRow<LookupCall<any>> {
+    let data = arrays.find(this.data, data => key instanceof data[0]);
+    if (!data) {
+      return null;
     }
-    let data = arrays.find(this.data, data => {
-      return data[0] === key;
-    });
-    if (data) {
-      deferred.resolve({
-        queryBy: QueryBy.KEY,
-        lookupRows: [this._dataToLookupRow(data)]
-      });
-    } else {
-      deferred.reject();
-    }
+    return this._dataToLookupRow(data);
   }
 
   protected override _data(): any[] {
     return [
-      [scout.create(LocaleLookupCall, {session: this.session}), 'jswidgets.LocaleLookupCall'],
-      [scout.create(RainbowLookupCall, {session: this.session}), 'jswidgets.RainbowLookupCall'],
-      [scout.create(SalutationLookupCall, {session: this.session}), 'jswidgets.SalutationLookupCall'],
-      [scout.create(WorldLookupCall, {session: this.session}), 'jswidgets.WorldLookupCall']
+      [LocaleLookupCall, 'LocaleLookupCall'],
+      [RainbowLookupCall, 'RainbowLookupCall'],
+      [SalutationLookupCall, 'SalutationLookupCall'],
+      [WorldLookupCall, 'WorldLookupCall']
     ];
   }
 }
