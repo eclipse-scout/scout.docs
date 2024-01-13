60ec53f650b92068a2ad91d34a9fe9ebfc20c178
Stephan Merkli <stephan.merkli@bsi-software.com>
2024-01-13 19:40:12 +0100

    Applications: replace .war module and use servlet contributors instead of web.xml
    .war module output was used within an application server.
    That module is replaced by a .zip module which will create an archive containing the directories conf, libs and bin.
    Such an output can be directly run via shell scripts.
    Another replacement is a .image module which will build and publish a docker image.
    
    369376

---
+++ code/contacts/org.eclipse.scout.contacts.server.app/src/main/java/org/eclipse/scout/contacts/server/app/ContactsServerServletContributors.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.scout.contacts.server.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.scout.contacts.server.ServerServletFilter;
+import org.eclipse.scout.rt.jetty.IServletContributor;
+import org.eclipse.scout.rt.jetty.IServletFilterContributor;
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.platform.util.StringUtility;
+import org.eclipse.scout.rt.server.ServiceTunnelServlet;
+import org.eclipse.scout.rt.server.admin.diagnostic.DiagnosticServlet;
+
+/**
+ * {@link IServletContributor} and {@link IServletFilterContributor} for contacts server.
+ */
+public final class ContactsServerServletContributors {
+
+  private ContactsServerServletContributors() {
+  }
+
+  @Order(10)
+  public static class AuthFilterContributor implements IServletFilterContributor {
+
+    @Override
+    public void contribute(ServletContextHandler handler) {
+      FilterHolder filter = handler.addFilter(ServerServletFilter.class, "/*", null);
+      // values needs to be defined relative to application root path (which isn't always the same as servlet root path)
+      List<String> filterExcludes = Arrays.asList("/jaxws/*");
+      filter.setInitParameter("filter-exclude", StringUtility.join("\n", filterExcludes));
+    }
+  }
+
+  @Order(10)
+  public static class ServiceTunnelServletContributor implements IServletContributor {
+
+    @Override
+    public void contribute(ServletContextHandler handler) {
+      handler.addServlet(ServiceTunnelServlet.class, "/process");
+    }
+  }
+
+  @Order(20)
+  public static class DiagnosticServletContributor implements IServletContributor {
+
+    @Override
+    public void contribute(ServletContextHandler handler) {
+      handler.addServlet(DiagnosticServlet.class, "/diagnostics");
+    }
+  }
+}
