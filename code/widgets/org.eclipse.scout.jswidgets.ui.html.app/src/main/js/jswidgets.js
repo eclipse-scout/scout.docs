05c7caf30c2fa9c1edfcd2aeb2144e50e11a38dd
Matthias Villiger <mvi@bsi-software.com>
2024-03-06 18:48:10 +0100

    Expose ConfigProperties to Scout JS
    Remove scout:version tag as it is now retrieved from config properties.
    
    373066

--- code/widgets/org.eclipse.scout.jswidgets.ui.html.app/src/main/js/jswidgets.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html.app/src/main/js/jswidgets.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,11 +8,13 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 import {App} from '@eclipse-scout/demo-jswidgets';
+import {access} from '@eclipse-scout/core';
+
+App.addBootstrapper(() => access.bootstrapSystem());
 
 new App().init({
   bootstrap: {
-    textsUrl: 'res/texts.json',
-    permissionsUrl: 'api/permissions'
+    textsUrl: 'res/texts.json'
   }
 });
 
