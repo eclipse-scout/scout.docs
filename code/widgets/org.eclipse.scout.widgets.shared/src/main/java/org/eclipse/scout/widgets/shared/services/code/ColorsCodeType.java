e56b4e793fffcaedd8c12baf3dafe3c982f3eb4a
Matthias Villiger <mvi@bsi-software.com>
2024-03-15 08:59:01 +0100

    Consistently move all ToDoFunctions in a mapping package

--- code/widgets/org.eclipse.scout.widgets.shared/src/main/java/org/eclipse/scout/widgets/shared/services/code/ColorsCodeType.java
+++ code/widgets/org.eclipse.scout.widgets.shared/src/main/java/org/eclipse/scout/widgets/shared/services/code/ColorsCodeType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,9 +21,9 @@ import org.eclipse.scout.rt.platform.classid.ClassId;
 import org.eclipse.scout.rt.platform.text.TEXTS;
 import org.eclipse.scout.rt.shared.services.common.code.AbstractCode;
 import org.eclipse.scout.rt.shared.services.common.code.AbstractCodeType;
-import org.eclipse.scout.rt.shared.services.common.code.AbstractCodeToDoFunction.ICodeTypeDoIdConverter;
 import org.eclipse.scout.rt.shared.services.common.code.CodeRow;
 import org.eclipse.scout.rt.shared.services.common.code.ICodeRow;
+import org.eclipse.scout.rt.shared.services.common.code.mapping.AbstractCodeToDoFunction.ICodeTypeDoIdConverter;
 
 /**
  * @author mzi
