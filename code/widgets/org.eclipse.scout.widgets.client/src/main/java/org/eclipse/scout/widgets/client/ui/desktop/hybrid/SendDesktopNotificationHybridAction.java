7ad57205dc5230ddfb651a3a22f56f1188e87492
Beat Schwarzentrub <bsh@bsiag.com>
2024-07-03 18:06:14 +0200

    Cleanup whitespace

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/desktop/hybrid/SendDesktopNotificationHybridAction.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/ui/desktop/hybrid/SendDesktopNotificationHybridAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -23,16 +23,16 @@ import org.eclipse.scout.rt.platform.status.Status;
 
 @HybridActionType(SendDesktopNotificationHybridAction.TYPE)
 public class SendDesktopNotificationHybridAction extends AbstractHybridAction<IDoEntity> {
+
   protected static final String TYPE = "SendDesktopNotification";
 
   @Override
   public void execute(IDoEntity data) {
     IHtmlContent message = HTML.fragment(HTML.bold("Scout Classic"), HTML.br(), "This DesktopNotification was sent by the server.");
-    IDesktop.CURRENT.get().addNotification(new DesktopNotification(new Status(
-        message.toHtml(), IStatus.INFO),
-        TimeUnit.SECONDS.toMillis(2),
-        true,
-        true,
-        null));
+    DesktopNotification notification = new DesktopNotification(new Status(message.toHtml(), IStatus.INFO))
+        .withDuration(TimeUnit.SECONDS.toMillis(2))
+        .withClosable(true)
+        .withHtmlEnabled(true);
+    IDesktop.CURRENT.get().addNotification(notification);
   }
 }
