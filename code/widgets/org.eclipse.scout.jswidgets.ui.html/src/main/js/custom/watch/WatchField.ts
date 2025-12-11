bb6ef0de580bc08f268d6b98926583f48f384025
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2025-12-11 14:26:46 +0100

    aria: make field status accessible
    The field status now gets the proper aria roles and can be accessed
    using tab.
    
    Also:
    - Support keyboard navigation for menus in tooltip
      Because a field status can open a tooltip with menus, the menus need
      to support keyboard-navigation
    - Menu: support selected state
      A context menu uses the css class 'selected' during keyboard
      navigation to highlight the current menu item. This breaks two things:
      - The regular selected state cannot be visualized correctly.
      - If a tooltip has a hierarchical menu, the menu is opened as context
        menu popup. Closing it again with ESC does not focus the parent menu
        item in the tooltip because the selected state is removed by the
        menu item.
    
    Changes:
    - The menu item now uses the class 'focused' instead of 'selected' to
      for the keyboard navigation.
    - The class 'selected' now properly visualizes the selected state of a
      menu item. The state is now also correctly synchronized between a
      context menu item and the original menu item.
    - If the context menu item only toggles a sub menu or a form popup, the
      selected state must not be synchronized (existing behavior). The logic
      in Menu.ts had to be changed slightly to support this case properly.
    - The tooltip now better visualizes the different menu item states with
      different severities
    
    394500

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/custom/watch/WatchField.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/custom/watch/WatchField.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,8 +21,12 @@ export class WatchField extends FormField {
   protected override _render() {
     // create the field container
     this.addContainer(this.$parent, 'watch-field');
+
     // create a label
     this.addLabel();
+
+    // create mandatory indicator
+    this.addMandatoryIndicator();
 
     // create a field
     let $field = this.$parent.appendDiv('content');
@@ -36,7 +40,7 @@ export class WatchField extends FormField {
     // layout
     htmlField.setLayout(new WatchFieldLayout(this));
 
-    this.addMandatoryIndicator();
+    // create status
     this.addStatus();
 
     // start watch
