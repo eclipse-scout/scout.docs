b5f45b0d4caddc9b7366cdec778a285b3bec33ff
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2026-02-02 16:34:27 +0100

    JsWidgets: add menu to insert many tree nodes
    443405

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/tree/TreeFormModel.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/tree/TreeFormModel.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -47,6 +47,11 @@ export default (): FormModel => ({
                   objectType: Menu,
                   text: '${textKey:AddChildNode}',
                   keyStroke: 'insert'
+                },
+                {
+                  id: 'AddManyNodes',
+                  objectType: Menu,
+                  text: '${textKey:AddManyNodes}'
                 },
                 {
                   id: 'DeleteNodeMenu',
@@ -153,6 +158,7 @@ export type TreeFormWidgetMap = {
   'Tree': Tree;
   'AddNodeMenu': Menu;
   'AddChildNodeMenu': Menu;
+  'AddManyNodes': Menu;
   'DeleteNodeMenu': Menu;
   'DeleteAllMenu': Menu;
   'DeleteAllNodesMenu': Menu;
