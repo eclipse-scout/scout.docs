8a3390f798f2102e8f702aac11df3791a4ff7892
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2026-06-09 10:16:36 +0200

    JsWidgets: add html field

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/valuefield/ValueFieldPropertiesBoxModel.ts
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/valuefield/ValueFieldPropertiesBoxModel.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,7 +21,8 @@ export default (): GroupBoxModel => ({
       id: 'ValueField',
       objectType: StringField,
       label: 'Value',
-      enabled: false
+      enabled: false,
+      maxLength: 100000
     },
     {
       id: 'DisplayTextField',
